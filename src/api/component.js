e5c8d98de677e75172f7df5c069f479ffda260a6
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-05-07 20:46:11 -0300

    API: Fixed component routes

--- src/api/component.js
+++ src/api/component.js
@@ -1,21 +1,21 @@
 import client from './client';
 
 export const createComponent = async (name, description) => {
-    await client.create('components/', {name, description});
+    await client.create('/components', {name, description});
 }
 
 export const readAllComponents = async () => {
-    await client.get(`components/`);
+    await client.get(`/components`);
 }
 
 export const readComponentById = async (id) => {
-    await client.get(`components/${id}`);
+    await client.get(`/components/${id}`);
 }
 
 export const updateComponent = async (id, name, description) => {
-    await client.patch(`components/${id}`, {name, description});
+    await client.patch(`/components/${id}`, {name, description});
 }
 
 export const deleteComponent = async (id) => {
-    await client.delete(`components/${id}`);
+    await client.delete(`/components/${id}`);
 }
