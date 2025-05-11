c49c659d32f3ed6154dff5f7870d97350853877c
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-05-11 20:57:57 -0300

    Pages: Implemented the start screen

--- src/pages/Public/start/Start.js
+++ src/pages/Public/start/Start.js
@@ -1,9 +1,20 @@
 import React from "react";
 import styles from "./Start.module.scss";
+import StartLogo from "../../../components/logos/startLogo/StartLogo.js";
+import NavTextButton from "../../../components/buttons/textButtons/navTextButton/NavTextButton.js";
 
 function Start() {
     return (
-        <></>
+        <div className={styles.screen}>
+            <div className={styles.logo}>
+                <StartLogo />
+            </div>
+            <div className={styles.options}>
+                <NavTextButton text="Login" route="/Login" />
+                <NavTextButton text="Sign Up" route="/signUp" />
+                <NavTextButton text="Guest" route="/home" />
+            </div>
+        </div>
     )
 }
 export default Start;
