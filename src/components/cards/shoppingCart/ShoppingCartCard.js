3473e7e707e9b547cae13ddc04fdc77a48bac398
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-06-14 21:41:12 -0300

    Made the cards clickable

--- src/components/cards/shoppingCart/ShoppingCartCard.js
+++ src/components/cards/shoppingCart/ShoppingCartCard.js
@@ -3,14 +3,17 @@ import styles from "./ShoppingCartCard.module.scss";
 import DeleteModal from "../../buttons/modal/deleteModal/DeleteModal.js";
 import Form from "../../data/forms/Form.js";
 import {deleteCart} from "../../../api/shoppingCart.js";
+import {Link} from "react-router-dom";
 
 function ShoppingCartCard({shoppingCart}) {
     return (
         <div className={styles.card} key={shoppingCart.id}>
-            <div className={styles.info}>
-                <h3 className={styles.name}>{shoppingCart.name}</h3>
-                <p className={styles.date}>Last modification: {(shoppingCart.updatedAt).toString().slice(0,10)}</p>
-            </div>
+            <Link to={`/shoppingCart/${shoppingCart.id}`} className={styles.info}>
+                <div>
+                    <h3 className={styles.name}>{shoppingCart.name}</h3>
+                    <p className={styles.date}>Last modification: {(shoppingCart.updatedAt).toString().slice(0,10)}</p>
+                </div>
+            </Link>
             <div className={styles.actions}>
                 <DeleteModal>
                     <Form
